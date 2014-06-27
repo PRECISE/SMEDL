a6780709c2b8dbc0fd356b44cf3b2956c30a8db2
Peter Gebhard <pgeb@seas.upenn.edu>
2014-06-27 12:34:27 -0400

    Added Semantics class to handle failing 'identifier' rule on keywords.

---
+++ src/smedl_semantics.py
@@ -0,0 +1,24 @@
+"""
+The SMEDL semantics.
+"""
+
+from grako.exceptions import FailedSemantics
+
+keywords = set([
+    'object', 'identity', 'opaque', 'state',
+    'events', 'imported', 'exported', 'error', 'scenarios',
+    'this', 'else', 'raise', 'atomic',
+    'when'])
+
+class SmedlSemantics(object):
+
+    def __init__(self, name):
+        self.__name = name
+
+    def identifier(self, ast):
+        if ast in keywords:
+            raise FailedSemantics('"%s" is a keyword' % str(ast))
+        return ast
+
+    def _default(self, ast):
+        return ast
