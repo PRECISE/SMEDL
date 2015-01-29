486f3404cb694071a5fb1b4f4f75c6ec28a612e8
Brad Palchesko <b-rad@seas546.wireless-pennnet.upenn.edu>
2015-01-29 16:31:26 -0500

    Added fsm and symboltable tests

---
+++ src/test_fsm.py
@@ -0,0 +1,93 @@
+from fsm import *
+import unittest
+
+class TestFSM(unittest.TestCase):
+
+    def setUp(self):
+        self.fsm = FSM()
+        self.s1 = State("s1")
+        self.s2 = State("s2")
+        self.t1 = Transition(self.s1, self.s2, "SomeEvent", "SomeCondition")
+        self.t2 = Transition(self.s2, self.s1, "SomeOtherEvent", "SomeOtherCondition")
+
+    def test_addState(self):
+        self.assertRaises(TypeError, self.fsm.addState, 1)
+        self.assertRaises(TypeError, self.fsm.addState, None)
+        self.assertRaises(TypeError, self.fsm.addState, dict())
+        self.assertRaises(TypeError, self.fsm.addState, "state")
+        self.fsm.addState(self.s1)
+        self.fsm.addState(self.s2)
+        self.assertEqual(2, len(self.fsm.states.keys()))
+        s1 = self.fsm.states['s1']
+        self.assertTrue(isinstance(s1, State))
+
+    def test_deleteState(self):
+        self.fsm.addState(self.s1)
+        self.fsm.addState(self.s2)
+        self.assertEqual(2, len(self.fsm.states.keys()))
+        self.fsm.deleteState(self.s2)
+        # self.fsm.deleteState(self.s2) KeyError if not found
+        self.assertEqual(1, len(self.fsm.states.keys()))
+        self.assertTrue(isinstance(self.fsm.states['s1'], State))
+        self.assertRaises(KeyError, self.fsm.getStateByName, 's2')
+
+    def test_stateExists(self):
+        self.fsm.addState(self.s1)
+        self.assertRaises(TypeError, self.fsm.stateExists, 1)
+        self.assertRaises(TypeError, self.fsm.stateExists, self.s1)
+        self.assertTrue(self.fsm.stateExists(self.s1.name))
+        self.assertFalse(self.fsm.stateExists(self.s2.name))   
+        self.fsm.addState(self.s2)  
+        self.assertTrue(self.fsm.stateExists(self.s2.name))  
+
+    def test_getStateByName(self):
+        self.fsm.addState(self.s1)
+        self.assertRaises(TypeError, self.fsm.getStateByName, 1)
+        self.assertRaises(TypeError, self.fsm.getStateByName, self.s1)
+        s1_found = self.fsm.getStateByName(self.s1.name)
+        self.assertTrue(isinstance(s1_found, State))
+        self.assertEqual('s1', s1_found.name)
+        self.assertRaises(KeyError, self.fsm.getStateByName, 's2')   
+
+    def test_addTransition(self):
+        self.fsm.addState(self.s1)
+        self.assertRaises(TypeError, self.fsm.addTransition, self.s1)
+        self.assertRaises(TypeError, self.fsm.addTransition, "a string")
+        self.fsm.addTransition(self.t1) 
+        # Failed - no boolean return, runtime error? 
+        self.assertFalse(self.fsm.transitions)
+        self.fsm.addState(self.s2)
+        self.fsm.addTransition(self.t1) 
+        self.assertEqual(1, len(self.fsm.transitions))
+        self.fsm.addTransition(self.t2) 
+        self.assertEqual(2, len(self.fsm.transitions))
+
+    def test_deleteTransition(self):
+        self.assertRaises(ValueError, self.fsm.deleteTransition, "garbage") 
+        self.fsm.addState(self.s1)   
+        self.fsm.addState(self.s2)
+        self.fsm.addTransition(self.t1) 
+        self.fsm.addTransition(self.t2) 
+        self.assertEqual(2, len(self.fsm.transitions))
+        self.fsm.deleteTransition(self.t1)  
+        self.assertEqual(1, len(self.fsm.transitions)) 
+        self.fsm.deleteTransition(self.t2) 
+        self.assertFalse(self.fsm.transitions)  
+
+    def test_getTransitionByAction(self):
+        self.assertRaises(TypeError, self.fsm.getTransitionsByAction, 1)        
+        self.assertRaises(TypeError, self.fsm.getTransitionsByAction, self.t1) 
+        self.fsm.addState(self.s1) 
+        self.fsm.addState(self.s2)
+        self.fsm.addTransition(self.t1) 
+        self.fsm.addTransition(self.t2)
+        self.assertEqual(2, len(self.fsm.transitions))
+        self.assertFalse(self.fsm.getTransitionsByAction("nothing")) 
+        self.assertEqual(1, len(self.fsm.getTransitionsByAction("SomeEvent")))
+        t3 = Transition(self.s2, self.s1, "SomeEvent", "SomeOtherCondition") 
+        self.fsm.addTransition(t3)
+        self.assertEqual(3, len(self.fsm.transitions))
+        self.assertEqual(2, len(self.fsm.getTransitionsByAction("SomeEvent")))             
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ src/test_smedl_symboltable.py
@@ -0,0 +1,75 @@
+from smedl_symboltable import smedlSymbolTable
+import unittest
+
+
+class TestSymbolTable(unittest.TestCase):
+
+    def setUp(self):
+        self.symbolTable = smedlSymbolTable()
+
+    def test_add(self):
+        self.assertFalse(self.symbolTable.keys())
+        self.symbolTable.add('one')
+        attributes = {'type':'a'}
+        self.symbolTable.add('two', attributes.copy())
+        attributes['datatype'] = 'b'
+        self.symbolTable.add('three', attributes.copy())
+        self.assertEqual(3, len(self.symbolTable.keys()))
+        self.assertFalse(self.symbolTable['one'].keys())
+        self.assertEqual(1, len(self.symbolTable['two'].keys()))
+        self.assertEqual(2, len(self.symbolTable['three'].keys()))
+        self.assertEqual('a', self.symbolTable['three']['type'])
+
+    def test_get(self):
+        self.symbolTable.add('one')
+        self.symbolTable.add('two', {'type':'a'})
+        one = self.symbolTable.get('one')
+        two = self.symbolTable.get('two')
+        two_attr = self.symbolTable.get('two', 'type')
+        self.assertFalse(one)
+        self.assertEqual(1, len(two))
+        self.assertEqual('a', two['type'])
+        self.assertEqual('a', two_attr)
+        # Should symbolTable.get handle KeyError? It currently passes the buck
+        self.assertRaises(KeyError, self.symbolTable.get, 'five')
+        self.assertRaises(KeyError, self.symbolTable.get, 'one', 'datatype')
+
+    def test_getSymbolsByType(self):
+        self.symbolTable.add('one')
+        self.symbolTable.add('two', {'type':'a'})
+        self.symbolTable.add('three', {'type':'a', 'datatype':'b'})
+        self.symbolTable.add('four', {'type':'b'})
+        type_a = self.symbolTable.getSymbolsByType('a')
+        type_b = self.symbolTable.getSymbolsByType('b')
+        type_c = self.symbolTable.getSymbolsByType('c')
+        self.assertFalse(type_c)
+        self.assertEqual(2, len(type_a))
+        self.assertEqual(1, len(type_b))
+        self.assertTrue('two' in type_a)
+        self.assertTrue('three' in type_a)
+        self.assertFalse('four' in type_a)
+        self.assertTrue('four' in type_b)
+
+    def test_update(self):
+        self.symbolTable.add('one')
+        self.symbolTable.update('one', 'type', 'a')
+        self.assertEqual('a', self.symbolTable.get('one', 'type'))
+        self.symbolTable.update('one', 'type', 'b')
+        self.assertEqual('b', self.symbolTable.get('one', 'type'))
+        self.symbolTable.update('one', 'datatype', 'c')
+        self.assertEqual('c', self.symbolTable.get('one', 'datatype'))
+
+    def test_delete(self):
+        self.symbolTable.add('one')
+        self.symbolTable.add('two', {'type':'a'})
+        self.assertEqual(2, len(self.symbolTable.keys()))
+        self.symbolTable.delete('two','type')
+        self.assertFalse(self.symbolTable.get('two','type'))
+        self.symbolTable.delete('one')
+        self.assertFalse(self.symbolTable.get('two','type'))
+        # Would have expected self(symbol).pop and self[symbol](attribute).pop
+        # to remove relative key, not just value
+        # self.assertEqual(1, len(self.symbolTable.keys()))
+
+if __name__ == '__main__':
+    unittest.main()
