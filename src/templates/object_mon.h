17126db6b06a89aa1d407d22a777f54d12242136
Peter Gebhard <pgeb@seas.upenn.edu>
2016-01-21 15:59:02 -0500

    Organized and added some minor comments.

--- src/templates/object_mon.h
+++ src/templates/object_mon.h
@@ -5,13 +5,13 @@
 #define {{obj|upper}}_MONITOR_MAP_SIZE 100 // number of buckets
 #define {{obj|upper}}_MONITOR_IDENTITIES {{identities|length}}
 
-typedef struct {{obj|title}}Data {  
+typedef struct {{obj|title}}Data {
 {{identity_declarations}}
 {{state_var_declarations}}
 } {{obj|title}}Data;
 
 typedef struct {{obj|title}}Monitor {
-  pthread_mutex_t monitor_lock;   
+  pthread_mutex_t monitor_lock;
   MonitorIdentity *identities[{{identities|length}}];
   int state[{{scenario_names|length}}];
 {{state_var_declarations}}
@@ -31,13 +31,21 @@ typedef struct {{obj|title}}MonitorMap {
 pthread_mutex_t {{obj|lower}}_monitor_maps_lock;
 
 {{obj|title}}Monitor* init_{{obj|lower}}_monitor({{obj|title}}Data*);
-int init_{{obj|lower}}_monitor_maps();
-int add_{{obj|lower}}_monitor_to_map({{obj|title}}Monitor*, int);
-int put_{{obj|lower}}_monitor({{obj|title}}Monitor*); //puts into all maps
+
+/*
+ * Monitor Event Handlers
+ */
+{{signatures|join(';\n')}};
+
+/*
+ * Monitor Utility Functions
+ */
 {{obj|title}}MonitorRecord* get_{{obj|lower}}_monitors();
 {{obj|title}}MonitorRecord* get_{{obj|lower}}_monitors_by_identity(int, int, void*);
 {{obj|title}}MonitorRecord* filter_{{obj|lower}}_monitors_by_identity({{obj|title}}MonitorRecord*, int, void*);
-{{signatures|join(';\n')}};
+int init_{{obj|lower}}_monitor_maps();
+int add_{{obj|lower}}_monitor_to_map({{obj|title}}Monitor*, int);
+int put_{{obj|lower}}_monitor({{obj|title}}Monitor*); //puts into all maps
 void raise_error(char*, const char*, char*, char*);
 void free_{{obj|lower}}_monitor();
 void free_{{obj|lower}}_monitor_maps();
