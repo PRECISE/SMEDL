bb17f49d7099ae44fe7444090f0c23b5ee2123f4
Peter Gebhard <pgeb@seas.upenn.edu>
2016-01-26 22:21:50 -0500

    Added spacing around template entries for better readability.

--- src/templates/object_mon.c
+++ src/templates/object_mon.c
@@ -3,26 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "{{base_file_name}}_mon.h"
-{%- if helper %}{{'\n'}}#include "{{helper}}"{% endif %}
+{%- if helper %}{{ '\n' }}#include "{{helper}}"{% endif %}
 
-typedef enum { {{identities_names|join(', ')}} } {{obj|lower}}_identity;
-const identity_type {{obj|lower}}_identity_types[{{obj|upper}}_MONITOR_IDENTITIES] = { {{ identities_types|join(', ') }} };
+typedef enum { {{ identities_names|join(', ') }} } {{ obj|lower }}_identity;
+const identity_type {{ obj|lower }}_identity_types[{{ obj|upper }}_MONITOR_IDENTITIES] = { {{ identities_types|join(', ') }} };
 
-typedef enum { {{ scenario_names|join(', ') }} } {{obj|lower}}_scenario;
-{{state_enums}}
-typedef enum { {{event_enums}} } {{obj|lower}}_event;
-typedef enum { {{error_enums}} } {{obj|lower}}_error;
-{{state_names}}
-const char **{{obj|lower}}_states_names[{{state_names_array|length}}] = { {{state_names_array|join(', ')}} };
+typedef enum { {{ scenario_names|join(', ') }} } {{ obj|lower }}_scenario;
+{{ state_enums }}
+typedef enum { {{ event_enums }} } {{ obj|lower }}_event;
+typedef enum { {{ error_enums }} } {{ obj|lower }}_error;
+{{ state_names }}
+const char **{{ obj|lower }}_states_names[{{ state_names_array|length }}] = { {{ state_names_array|join(', ') }} };
 
-{{obj|title}}Monitor* init_{{obj|lower}}_monitor( {{obj|title}}Data *d ) {
-    {{obj|title}}Monitor* monitor = ({{obj|title}}Monitor*)malloc(sizeof({{obj|title}}Monitor));
+{{ obj|title }}Monitor* init_{{ obj|lower }}_monitor( {{ obj|title }}Data *d ) {
+    {{ obj|title }}Monitor* monitor = ({{ obj|title }}Monitor*)malloc(sizeof({{ obj|title }}Monitor));
     pthread_mutex_init(&monitor->monitor_lock, NULL);
-{% for id in identities %}    monitor->identities[{{obj|upper}}_{{id.name|upper}}] = init_monitor_identity({{id.type|upper}}, {% if id.type|upper == "INT" %}&{% endif -%}d->{{id.name}});
+{% for id in identities %}    monitor->identities[{{ obj|upper }}_{{ id.name|upper }}] = init_monitor_identity({{ id.type|upper }}, {% if id.type|upper == "INT" %}&{% endif -%}d->{{ id.name }});
 {% endfor -%}
-{% for v in state_vars %}    monitor->{{v.name|lower}} = d->{{v.name|lower}};
+{% for v in state_vars %}    monitor->{{ v.name }} = d->{{ v.name }};
 {% endfor %}{{state_inits}}
-    put_{{obj|lower}}_monitor(monitor);
+    put_{{ obj|lower }}_monitor(monitor);
     return monitor;
 }
 
@@ -32,9 +32,9 @@ const char **{{obj|lower}}_states_names[{{state_names_array|length}}] = { {{stat
  */
 
 {% for e in event_code -%}
-{{e.event|join('\n')}}
-{% if e.probe %}{{'\n'}}{{e.probe|join('\n')}}{{'\n'}}{% endif %}
-{{e.raise|join('\n')}}
+{{ e.event|join('\n') }}
+{% if e.probe %}{{ '\n' }}{{ e.probe|join('\n') }}{{ '\n' }}{% endif %}
+{{ e.raise|join('\n') }}
 {% endfor -%}
 
 
@@ -42,46 +42,46 @@ const char **{{obj|lower}}_states_names[{{state_names_array|length}}] = { {{stat
  * Monitor Utility Functions
  */
 
-int init_{{obj|lower}}_monitor_maps() {
-    if (pthread_mutex_init(&{{obj|lower}}_monitor_maps_lock, NULL) != 0) {
-        printf("\n{{obj|title}} Monitor map mutex init failed\n");
+int init_{{ obj|lower }}_monitor_maps() {
+    if (pthread_mutex_init(&{{ obj|lower }}_monitor_maps_lock, NULL) != 0) {
+        printf("\n{{ obj|title }} Monitor map mutex init failed\n");
         return 0;
     }
-    for(int i = 0; i < {{obj|upper}}_MONITOR_IDENTITIES; i++) {
-        {{obj|lower}}_monitor_maps[i] = ({{obj|title}}MonitorMap*)malloc(sizeof({{obj|title}}MonitorMap));
+    for(int i = 0; i < {{ obj|upper }}_MONITOR_IDENTITIES; i++) {
+        {{ obj|lower }}_monitor_maps[i] = ({{ obj|title }}MonitorMap*)malloc(sizeof({{ obj|title }}MonitorMap));
     }
     return 1;
 }
 
-int add_{{obj|lower}}_monitor_to_map({{obj|title}}Monitor *monitor, int identity) {
-    {{obj|title}}MonitorMap* map = {{obj|lower}}_monitor_maps[identity];
+int add_{{ obj|lower }}_monitor_to_map({{ obj|title }}Monitor *monitor, int identity) {
+    {{ obj|title }}MonitorMap* map = {{ obj|lower }}_monitor_maps[identity];
     int bucket = hash_monitor_identity(monitor->identities[identity]->type,
-        monitor->identities[identity]->value, {{obj|upper}}_MONITOR_MAP_SIZE);
-    {{obj|title}}MonitorRecord* record = ({{obj|title}}MonitorRecord*)malloc(sizeof({{obj|title}}MonitorRecord));
+        monitor->identities[identity]->value, {{ obj|upper }}_MONITOR_MAP_SIZE);
+    {{ obj|title }}MonitorRecord* record = ({{ obj|title }}MonitorRecord*)malloc(sizeof({{ obj|title }}MonitorRecord));
     if(monitor == NULL || record == NULL) {
         free(monitor);
         free(record);
         return 0;
     }
     record->monitor = monitor;
-    pthread_mutex_lock(&{{obj|lower}}_monitor_maps_lock);
+    pthread_mutex_lock(&{{ obj|lower }}_monitor_maps_lock);
     record->next = map->list[bucket];
     map->list[bucket] = record;
-    pthread_mutex_unlock(&{{obj|lower}}_monitor_maps_lock);
+    pthread_mutex_unlock(&{{ obj|lower }}_monitor_maps_lock);
     return 1;
 }
 
-int put_{{obj|lower}}_monitor({{obj|title}}Monitor *monitor) {
-    return {{add_to_map_calls|join(' &&\n      ')}};
+int put_{{ obj|lower }}_monitor({{ obj|title }}Monitor *monitor) {
+    return {{ add_to_map_calls|join(' &&\n      ') }};
 }
 
-{{obj|title}}MonitorRecord* get_{{obj|lower}}_monitors() {
-    {{obj|title}}MonitorRecord* results = NULL;
-    {{obj|title}}MonitorMap* map = {{obj|lower}}_monitor_maps[0];
-    for(int i = 0; i < {{obj|upper}}_MONITOR_MAP_SIZE; i++) {
-        {{obj|title}}MonitorRecord* current = map->list[i];
+{{ obj|title }}MonitorRecord* get_{{ obj|lower }}_monitors() {
+    {{ obj|title }}MonitorRecord* results = NULL;
+    {{ obj|title }}MonitorMap* map = {{ obj|lower }}_monitor_maps[0];
+    for(int i = 0; i < {{ obj|upper }}_MONITOR_MAP_SIZE; i++) {
+        {{ obj|title }}MonitorRecord* current = map->list[i];
         while(current != NULL) {
-            {{obj|title}}MonitorRecord* record = ({{obj|title}}MonitorRecord*)malloc(sizeof({{obj|title}}MonitorRecord));
+            {{ obj|title }}MonitorRecord* record = ({{ obj|title }}MonitorRecord*)malloc(sizeof({{ obj|title }}MonitorRecord));
             record->monitor = current->monitor;
             record->next = results;
             results = record;
@@ -91,14 +91,14 @@ int put_{{obj|lower}}_monitor({{obj|title}}Monitor *monitor) {
     return results;
 }
 
-{{obj|title}}MonitorRecord* get_{{obj|lower}}_monitors_by_identity(int identity, int type, void *value) {
-    {{obj|title}}MonitorRecord* results = NULL;
-    {{obj|title}}MonitorMap* map = {{obj|lower}}_monitor_maps[identity];
-    int bucket = hash_monitor_identity(type, value, {{obj|upper}}_MONITOR_MAP_SIZE);
-    {{obj|title}}MonitorRecord* current = map->list[bucket];
+{{ obj|title }}MonitorRecord* get_{{obj|lower}}_monitors_by_identity(int identity, int type, void *value) {
+    {{ obj|title }}MonitorRecord* results = NULL;
+    {{ obj|title }}MonitorMap* map = {{ obj|lower }}_monitor_maps[identity];
+    int bucket = hash_monitor_identity(type, value, {{ obj|upper }}_MONITOR_MAP_SIZE);
+    {{ obj|title }}MonitorRecord* current = map->list[bucket];
     while(current != NULL) {
         if(compare_monitor_identity(value, current->monitor->identities[identity])) {
-            {{obj|title}}MonitorRecord* record = ({{obj|title}}MonitorRecord*)malloc(sizeof({{obj|title}}MonitorRecord));
+            {{ obj|title }}MonitorRecord* record = ({{ obj|title }}MonitorRecord*)malloc(sizeof({{ obj|title }}MonitorRecord));
             record->monitor = current->monitor;
             record->next = results;
             results = record;
@@ -108,11 +108,11 @@ int put_{{obj|lower}}_monitor({{obj|title}}Monitor *monitor) {
     return results;
 }
 
-{{obj|title}}MonitorRecord* filter_{{obj|lower}}_monitors_by_identity({{obj|title}}MonitorRecord* before, int identity, void  *value) {
-    {{obj|title}}MonitorRecord* results = NULL;
+{{ obj|title }}MonitorRecord* filter_{{ obj|lower }}_monitors_by_identity({{ obj|title }}MonitorRecord* before, int identity, void  *value) {
+    {{ obj|title }}MonitorRecord* results = NULL;
     while(before != NULL) {
         if(compare_monitor_identity(value, before->monitor->identities[identity])) {
-            {{obj|title}}MonitorRecord* record = ({{obj|title}}MonitorRecord*)malloc(sizeof({{obj|title}}MonitorRecord));
+            {{ obj|title }}MonitorRecord* record = ({{ obj|title }}MonitorRecord*)malloc(sizeof({{ obj|title }}MonitorRecord));
             record->monitor = before->monitor;
             record->next = results;
             results = record;
--- src/templates/object_mon.h
+++ src/templates/object_mon.h
@@ -1,51 +1,51 @@
 #include "monitor_map.h"
 #include "actions.h"
-{%- if multithreaded %}{{'\n'}}#include <pthread.h>{% endif %}
+{%- if multithreaded %}{{ '\n' }}#include <pthread.h>{% endif %}
 
-#define {{obj|upper}}_MONITOR_MAP_SIZE 100 // number of buckets
-#define {{obj|upper}}_MONITOR_IDENTITIES {{identities|length}}
+#define {{ obj|upper }}_MONITOR_MAP_SIZE 100 // number of buckets
+#define {{ obj|upper }}_MONITOR_IDENTITIES {{ identities|length }}
 
-typedef struct {{obj|title}}Data {
-{{identity_declarations}}
-{{state_var_declarations}}
-} {{obj|title}}Data;
+typedef struct {{ obj|title }}Data {
+{{ identity_declarations }}
+{{ state_var_declarations }}
+} {{ obj|title }}Data;
 
-typedef struct {{obj|title}}Monitor {
+typedef struct {{ obj|title }}Monitor {
   pthread_mutex_t monitor_lock;
-  MonitorIdentity *identities[{{identities|length}}];
-  int state[{{scenario_names|length}}];
-{{state_var_declarations}}
+  MonitorIdentity *identities[{{ identities|length }}];
+  int state[{{ scenario_names|length }}];
+{{ state_var_declarations }}
   action *action_queue;
-} {{obj|title}}Monitor;
+} {{ obj|title }}Monitor;
 
-typedef struct {{obj|title}}MonitorRecord {
-  {{obj|title}}Monitor *monitor;
-  struct {{obj|title}}MonitorRecord *next;
-} {{obj|title}}MonitorRecord;
+typedef struct {{ obj|title }}MonitorRecord {
+  {{ obj|title }}Monitor *monitor;
+  struct {{ obj|title }}MonitorRecord *next;
+} {{ obj|title }}MonitorRecord;
 
-typedef struct {{obj|title}}MonitorMap {
-  {{obj|title}}MonitorRecord *list[{{obj|upper}}_MONITOR_MAP_SIZE];
-} {{obj|title}}MonitorMap;
+typedef struct {{ obj|title }}MonitorMap {
+  {{ obj|title }}MonitorRecord *list[{{ obj|upper }}_MONITOR_MAP_SIZE];
+} {{ obj|title }}MonitorMap;
 
-{{obj|title}}MonitorMap* {{obj|lower}}_monitor_maps[{{obj|upper}}_MONITOR_IDENTITIES]; //a map for each identity
-pthread_mutex_t {{obj|lower}}_monitor_maps_lock;
+{{ obj|title }}MonitorMap* {{ obj|lower }}_monitor_maps[{{ obj|upper }}_MONITOR_IDENTITIES]; //a map for each identity
+pthread_mutex_t {{ obj|lower }}_monitor_maps_lock;
 
-{{obj|title}}Monitor* init_{{obj|lower}}_monitor({{obj|title}}Data*);
+{{ obj|title }}Monitor* init_{{ obj|lower }}_monitor({{ obj|title }}Data*);
 
 /*
  * Monitor Event Handlers
  */
-{{signatures|join(';\n')}};
+{{ signatures|join(';\n') }};
 
 /*
  * Monitor Utility Functions
  */
-{{obj|title}}MonitorRecord* get_{{obj|lower}}_monitors();
-{{obj|title}}MonitorRecord* get_{{obj|lower}}_monitors_by_identity(int, int, void*);
-{{obj|title}}MonitorRecord* filter_{{obj|lower}}_monitors_by_identity({{obj|title}}MonitorRecord*, int, void*);
-int init_{{obj|lower}}_monitor_maps();
-int add_{{obj|lower}}_monitor_to_map({{obj|title}}Monitor*, int);
-int put_{{obj|lower}}_monitor({{obj|title}}Monitor*); //puts into all maps
+{{ obj|title }}MonitorRecord* get_{{ obj|lower }}_monitors();
+{{ obj|title }}MonitorRecord* get_{{ obj|lower }}_monitors_by_identity(int, int, void*);
+{{ obj|title }}MonitorRecord* filter_{{ obj|lower }}_monitors_by_identity({{ obj|title }}MonitorRecord*, int, void*);
+int init_{{ obj|lower }}_monitor_maps();
+int add_{{ obj|lower }}_monitor_to_map({{ obj|title }}Monitor*, int);
+int put_{{ obj|lower }}_monitor({{ obj|title }}Monitor*); //puts into all maps
 void raise_error(char*, const char*, char*, char*);
-void free_{{obj|lower}}_monitor();
-void free_{{obj|lower}}_monitor_maps();
+void free_{{ obj|lower }}_monitor();
+void free_{{ obj|lower }}_monitor_maps();
