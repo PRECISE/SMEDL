c64aecb0f1ad82d373ff5b18387086de4fdbb44e
Peter Gebhard <pgeb@seas.upenn.edu>
2016-05-10 13:57:30 -0400

    Using czmq for broker, added capture of messages.

--- zeromq/broker.c
+++ zeromq/broker.c
@@ -1,30 +1,67 @@
 //  Simple message queuing broker
 //  Same as request-reply broker but using shared queue proxy
 //  See: http://zguide.zeromq.org/page:all#ZeroMQ-s-Built-In-Proxy-Function
-//  Compile (OS X): gcc broker.c -L/usr/local/lib -lzmq -I/usr/local/include -o broker
+//  Compile (OS X): gcc broker.c -L/usr/local/lib -lczmq -I/usr/local/include -o broker
 
-#include "zhelpers.h"
+#include "czmq.h"
+
+static int
+capture_event (zloop_t *loop, zsock_t *reader, void *arg)
+{
+    zmsg_t *msg = zmsg_recv (reader);
+    assert (msg);
+    char *msg_str = zmsg_popstr (msg);
+    printf ("CAPTURED: %s\n", msg_str);
+    free (msg_str);
+    zmsg_destroy (&msg);
+    return 0;
+}
 
 int main (void)
 {
-    void *context = zmq_ctx_new ();
+    bool verbose = true;
 
-    //  Socket facing publishers
-    void *frontend = zmq_socket (context, ZMQ_XSUB);
-    int rc = zmq_bind (frontend, "tcp://*:5559");
+    // Create and configure our proxy
+    zactor_t *proxy = zactor_new (zproxy, NULL);
+    assert (proxy);
+    if (verbose) {
+     zstr_sendx (proxy, "VERBOSE", NULL);
+     zsock_wait (proxy);
+    }
+    zstr_sendx (proxy, "FRONTEND", "XSUB", "tcp://*:5559", NULL);
+    zsock_wait (proxy);
+    zstr_sendx (proxy, "BACKEND", "XPUB", "tcp://*:5560", NULL);
+    zsock_wait (proxy);
+
+    // Test capture functionality
+    zsock_t *capture = zsock_new_pull ("inproc://capture");
+    assert (capture);
+
+    // Switch on capturing, check that it works
+    zstr_sendx (proxy, "CAPTURE", "inproc://capture", NULL);
+    zsock_wait (proxy);
+
+    // Setup loop for captures
+    zloop_t *loop = zloop_new ();
+    assert (loop);
+    zloop_set_verbose (loop, verbose);
+
+    // Set up reader for captures
+    int rc = zloop_reader (loop, capture, capture_event, NULL);
     assert (rc == 0);
+    zloop_reader_set_tolerant (loop, capture);
+    zloop_start (loop);
 
-    //  Socket facing subscribers
-    void *backend = zmq_socket (context, ZMQ_XPUB);
-    rc = zmq_bind (backend, "tcp://*:5560");
-    assert (rc == 0);
+    // Loop indefinitely
+    //while (1) {}
 
-    //  Start the proxy
-    zmq_proxy (frontend, backend, NULL);
+    // cleanup (never reached)
+    zloop_destroy (&loop);
+    assert (loop == NULL);
+    zsock_destroy (&capture);
+    assert (capture == NULL);
+    zactor_destroy (&proxy);
+    assert (proxy == NULL);
 
-    //  We never get here…
-    zmq_close (frontend);
-    zmq_close (backend);
-    zmq_ctx_destroy (context);
     return 0;
 }
