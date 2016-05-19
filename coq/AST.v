b280b21fc11c8d765e145f50f12ef80b67b5e02f
Theo Giannakopoulos <theo.giannakopoulos@baesystems.com>
2016-05-19 15:19:43 -0400

    trying to get coq-smedl to have nicer definitions

--- coq/AST.v
+++ coq/AST.v
@@ -1,7 +1,7 @@
 Require Import Coq.Lists.List.
 Require Import Coq.QArith.QArith.
 Require Import Coq.Strings.String.
-Require Import Types.
+Require Import SMEDL.Types.
 
 Open Scope type.
 
@@ -17,6 +17,7 @@ Inductive UnOp :=
 | Neg : UnOp.
            
 Inductive BinOp :=
+| Eq : BinOp
 | And : BinOp
 | Or : BinOp
 | Plus : BinOp
@@ -32,7 +33,9 @@ Inductive Expr :=
 
 Inductive Cmd :=
 | Assign : string -> Expr -> Cmd
-| Raise : string -> list Expr -> Cmd.
+| Raise : string -> list Expr -> Cmd
+| If : Expr -> Cmd -> Cmd -> Cmd
+| Seq : Cmd -> Cmd -> Cmd.
 
 Record Transition := mkTransition
   { transitionSourceState : string;
@@ -44,3 +47,41 @@ Record Transition := mkTransition
 Definition Scenario := list Transition.
 
 Definition Monitor := list Scenario.
+
+Definition show_UnOp (op : UnOp) : string :=
+  match op with
+  | Not => "~"
+  | Neg => "-"
+  end.
+
+Definition show_BinOp (op : BinOp) : string :=
+  match op with
+  | Eq => "="
+  | And => "&"
+  | Or => "|"
+  | Plus => "+"
+  | Times => "*"
+  end.
+
+Fixpoint show_Expr (e : Expr) : string :=
+  match e with
+  | Var v => v                  (* TODO *)
+  | LitInt n => "0"             (* TODO *)
+  | LitFloat q => "0.0"         (* TODO *)
+  | LitBool b => "#t"           (* TODO *)
+  | UnOpExpr op body =>
+    show_UnOp op ++ show_Expr body
+  | BinOpExpr op lhs rhs =>
+    "(" ++ show_Expr lhs ++ show_BinOp op ++ show_Expr rhs ++ ")"
+  end.
+
+Fixpoint show_Cmd (c : Cmd) : string :=
+  match c with
+  | Assign v e => v ++ ":=" ++ show_Expr e
+  | Raise e args => "raise " ++ e ++ "args" (* TODO *)
+  | If cond ctrue cfalse =>
+    "if (" ++ show_Expr cond ++ ") then {\n"
+           ++ show_Cmd ctrue ++ "} else {\n"
+           ++ show_Cmd cfalse ++ "}"
+  | Seq c1 c2 => show_Cmd c1 ++ ";\n" ++ show_Cmd c2
+  end.
--- coq/Elaborate.v
+++ coq/Elaborate.v
@@ -1,115 +1,178 @@
 Require Import Coq.Lists.List.
 Require Import Coq.QArith.QArith.
 Require Import Coq.Strings.String.
-Require Import Types.
-Require Import AST.
-Require Import ElaboratedAST.
+Require Import Coq.Program.Program.
+
+Require Import SMEDL.Types.
+Require SMEDL.AST.
+Require Import SMEDL.ElaboratedAST.
+Require Import SMEDL.These.
 
 Open Scope type.
+Open Scope string.
+Open Scope program_scope.
 
-Definition bind {a b : Type} (f : a -> option b) (x : option a) : option b :=
-  match x with
-  | None => None
-  | Some x' => f x'
-  end.
-
-Definition bind2 {a b c : Type} (f : a -> b -> option c) (x : option a) (y : option b) : option c :=
-  match (x, y) with
-  | (None, _) => None
-  | (_, None) => None
-  | (Some x', Some y') => f x' y'
-  end.
+Definition TypeError := string.
+Definition TC a := Chronicle TypeError a.
 
 Arguments existT {_} {_} _ _.
 Arguments exist {_} {_} _ _.
 
-Definition check_UnOpExpr {free_vars : list string}
-           (env : var free_vars -> Ty)
-           (op : AST.UnOp)
-           (e : { ty : Ty & ElaboratedAST.Expr env ty}) :
-  option { ty : Ty & ElaboratedAST.Expr env ty } :=
-  match (op, e) with
-  | (AST.Not, existT SBool e') =>
-    Some (existT SBool (UnOpExpr Not e'))
-  | (AST.Neg, existT SInt e') =>
-    Some (existT SInt (UnOpExpr INeg e'))
-  | (AST.Neg, existT SFloat e') =>
-    Some (existT SFloat (UnOpExpr FNeg e'))
-  | _ => None
+Program Definition check_UnOpExpr
+        {ty : Ty}
+        {free_vars : list string}
+        (env : var free_vars -> Ty)
+        (op : AST.UnOp)
+        (e : Expr env ty) :
+  TC { ty : Ty & Expr env ty } :=
+  match (op, ty) with
+  | (AST.Not, SBool) =>
+    ret (existT SBool (UnOpExpr Not e))
+  | (AST.Neg, SInt) =>
+    ret (existT SInt (UnOpExpr INeg e))
+  | (AST.Neg, SFloat) =>
+    ret (existT SFloat (UnOpExpr FNeg e))
+  | (op, _) =>
+    confess ("Unary operation "
+               ++ AST.show_UnOp op
+               ++ " cannot apply to expression of type "
+               ++ show_Ty ty ++ ".")
   end.
+Solve Obligations with (simpl; intuition; congruence).
 
-Definition check_BinOpExpr {free_vars : list string}
-                (env : var free_vars -> Ty)
-                (op : AST.BinOp)
-                (lhs : { ty : Ty & ElaboratedAST.Expr env ty})
-                (rhs : { ty : Ty & ElaboratedAST.Expr env ty }) :
-  option { ty : Ty & ElaboratedAST.Expr env ty } :=
-  match (op, lhs, rhs) with
-  | (AST.And, existT SBool lhs', existT SBool rhs') =>
-    Some (existT SBool (BinOpExpr And lhs' rhs'))
-  | (AST.Or, existT SBool lhs', existT SBool rhs') =>
-    Some (existT SBool (BinOpExpr Or lhs' rhs'))
-  | (AST.Plus , existT SInt lhs', existT SInt rhs') =>
-    Some (existT SInt (BinOpExpr IPlus lhs' rhs'))
-  | (AST.Times , existT SInt lhs', existT SInt rhs') =>
-    Some (existT SInt (BinOpExpr ITimes lhs' rhs'))
-  | (AST.Plus , existT SFloat lhs', existT SFloat rhs') =>
-    Some (existT SFloat (BinOpExpr FPlus lhs' rhs'))
-  | (AST.Times , existT SFloat lhs', existT SFloat rhs') =>
-    Some (existT SFloat (BinOpExpr FTimes lhs' rhs'))
-  | _ => None
+Program Definition check_BinOpExpr
+        {free_vars : list string}
+        {ty_lhs : Ty}
+        {ty_rhs : Ty}
+        (env : var free_vars -> Ty)
+        (op : AST.BinOp)
+        (lhs : Expr env ty_lhs)
+        (rhs : Expr env ty_rhs) :
+  TC { ty : Ty & Expr env ty } :=
+  match (op, ty_lhs, ty_rhs) with
+  | (AST.And, SBool, SBool) =>
+    ret (existT SBool (BinOpExpr And lhs rhs))
+  | (AST.Or, SBool, SBool) =>
+    ret (existT SBool (BinOpExpr Or lhs rhs))
+  | (AST.Plus, SInt, SInt) =>
+    ret (existT SInt (BinOpExpr IPlus lhs rhs))
+  | (AST.Times, SInt, SInt) =>
+    ret (existT SInt (BinOpExpr ITimes lhs rhs))
+  | (AST.Plus, SFloat, SFloat) =>
+    ret (existT SFloat (BinOpExpr FPlus lhs rhs))
+  | (AST.Times,SFloat, SFloat) =>
+    ret (existT SFloat (BinOpExpr FTimes lhs rhs))
+  | (op, _, _) =>
+    confess ("Binary operation "
+               ++ AST.show_BinOp op
+               ++ " cannot apply to expressions of type "
+               ++ show_Ty ty_lhs ++ " and "
+               ++ show_Ty ty_rhs ++ ".")
   end.
+Solve Obligations with (simpl; intuition congruence).
 
 Fixpoint check_Expr {free_vars : list string}
          (env : var free_vars -> Ty)
          (e : AST.Expr) :
-  option { ty : Ty & ElaboratedAST.Expr env ty } :=
+  TC { ty : Ty & Expr env ty } :=
   match e with
   | AST.Var v => match in_dec string_dec v free_vars with
-                 | left pf => Some (existT (env (exist v pf))
-                                           (Var (exist v pf)))
-                 | right _ => None
+                 | left pf =>
+                   ret (existT (env (exist v pf))
+                               (Var (exist v pf)))
+                 | right _ =>
+                   confess ("Variable "
+                              ++ v ++ " not in scope")
                  end
   | AST.LitInt n =>
-    Some (existT SInt (ElaboratedAST.LitInt n))
+    ret (existT SInt (LitInt n))
   | AST.LitFloat q =>
-    Some (existT SFloat (ElaboratedAST.LitFloat q))
+    ret (existT SFloat (LitFloat q))
   | AST.LitBool b =>
-    Some (existT SBool (ElaboratedAST.LitBool b))
-  | AST.UnOpExpr op e' =>
-    bind (check_UnOpExpr env op) (check_Expr env e')
+    ret (existT SBool (LitBool b))
+  | AST.UnOpExpr op body =>
+    do (ty, body') <- check_Expr env body;
+       check_UnOpExpr env op body'
   | AST.BinOpExpr op lhs rhs =>
-    bind2 (check_BinOpExpr env op) (check_Expr env lhs) (check_Expr env rhs)
+    do (lhs_ty, lhs') <- check_Expr env lhs;
+    do (rhs_ty, rhs') <- check_Expr env rhs;
+       check_BinOpExpr env op lhs' rhs'
   end.
 
-Fixpoint check_Cmd
+Program Fixpoint check_Cmd
          {globals : list string}
-         (global_env : var globals -> Ty)
          {events : list string}
          {event_params : event events -> list string}
+         (global_env : var globals -> Ty)
          (event_env : forall (e : event events), event_param events event_params e -> Ty)
-         (e : AST.Cmd) :
-  option (ElaboratedAST.Cmd global_env event_env) :=
-  match e with
+         (c : AST.Cmd) :
+  TC (Cmd global_env event_env) :=
+  match c with
   | AST.Assign v e =>
     match in_dec string_dec v globals with
-    | right _ => None
-    | left pf => match check_Expr global_env e with
-                 | None => None
-                 | Some (existT actual e') =>
-                   let expected := global_env (exist v pf) in
-                   match ty_dec expected actual with
-                   | right _ => None
-                   | left refl =>
-                     Some (Assign event_env
-                                  (exist v pf)
-                                  (eq_rec_r _ e' refl))
-                   end
-                 end
+    | right _ =>
+      confess ("Variable " ++ v ++ " not in scope")
+    | left pf =>
+      do (e_ty, e') <- check_Expr _ e;
+      let expected := global_env (exist v pf) in
+      match ty_dec expected e_ty with
+      | right _ =>
+        confess ("Assignment to variable "
+                   ++ v ++ " expects type "
+                   ++ show_Ty expected ++ ", but "
+                   ++ show_Ty e_ty ++ " provided.")
+      | left refl =>
+        ret (Assign (exist v pf)
+                    (eq_rec_r _ e' refl))
+      end
     end
-  | AST.Raise f e =>
-    match in_dec string_dec f events with
-    | right _ => None
-    | left pf => None           (* TODO *)
+  | AST.Raise e args =>
+    match in_dec string_dec e events with
+    | right _ =>
+      confess ("Event " ++ e ++ " not in scope.")
+    | left pf =>
+      match tt with
+      | _ =>
+        ret (Raise e (fun p => _))
+      end
     end
+  | AST.If cond c1 c2 =>
+    do (cond_ty, cond') <- check_Expr _ cond ;
+       match cond_ty with
+       | SBool =>
+         do c1' <- check_Cmd _ _ c1 ;
+         do c2' <- check_Cmd _ _ c2 ;
+            ret (If cond' c1' c2')
+       | _ =>
+         confess ("Condition expression has type "
+                    ++ show_Ty cond_ty
+                    ++ ", but type SBool expected.")
+       end
+  | AST.Seq c1 c2 =>
+    do c1' <- check_Cmd _ _ c1 ;
+    do c2' <- check_Cmd _ _ c2 ;
+       ret (Seq c1' c2')
   end.
+Obligation 2.
+compare (List.length args)
+        (List.length (event_params (exist e pf)));
+  intros.
+apply (map (check_Expr global_env)) in args as args'.
+Check nth.
+Check In_nth.
+assert (Hin := In_nth (event_params (exist e pf)) p "").
+inversion Hin.
+
+nth_in_or_default:
+  forall (A : Type) (n : nat) (l : list A) (d : A),
+  {In (nth n l d) l} + {nth n l d = d}
+
+
+nth_In:
+  forall (A : Type) (n : nat) (l : list A) (d : A),
+  (n < Datatypes.length l)%nat -> In (nth n l d) l
+
+assert (event_env' := event_env (exist e pf)).
+assert (p_ty := event_env' p).
+
+SearchAbout In.
