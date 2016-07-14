d299f9678baaf8e3ac717739e8821e0062fcb77f
Peter Gebhard <pgeb@seas.upenn.edu>
2016-07-14 16:47:10 -0400

    Fixed actual monitor_map.c template file.

--- smedl/templates/monitor_map.c
+++ smedl/templates/monitor_map.c
@@ -5,25 +5,30 @@
 
 MonitorIdentity* init_monitor_identity(identity_type type, void *value) {
     MonitorIdentity* identity = (MonitorIdentity*)malloc(sizeof(MonitorIdentity));
+    int *i = (int*)malloc(sizeof(int));
+    uintptr_t *p = (uintptr_t*)malloc(sizeof(uintptr_t));
+    pthread_t *t = (pthread_t*)malloc(sizeof(pthread_t));
+
     identity->type = type;
+
     switch (type) {
-        case INT:;
-            int *i = (int*)malloc(sizeof(int));
+        case INT:
+        case OPAQUE:
             *i = *(int*)value;
             identity->value = i;
             break;
-        case POINTER:;
-            uintptr_t *p = (uintptr_t*)malloc(sizeof(uintptr_t));
+        case POINTER:
             *p = (uintptr_t)value;
             identity->value = p;
             break;
-        case THREAD:;
-            pthread_t *t = (pthread_t*)malloc(sizeof(pthread_t));
+        case THREAD:
             *t = *(pthread_t*)value;
             identity->value = t;
+            break;
         default:
             break;
     }
+
     return identity;
 }
 
@@ -31,6 +36,7 @@ int compare_monitor_identity(void *value, MonitorIdentity *other) {
     int value_match = 0;
     switch (other->type) {
         case INT:
+        case OPAQUE:
             if(*(int*)value == *(int*)other->value) {
                 value_match = 1;
             }
@@ -44,6 +50,7 @@ int compare_monitor_identity(void *value, MonitorIdentity *other) {
             if(*(pthread_t*)value == *(pthread_t*)other->value) {
                 value_match = 1;
             }
+            break;
         default:
             break;
     }
@@ -54,6 +61,7 @@ int hash_monitor_identity(identity_type type, void *value, int map_size) {
     int bucket;
     switch (type) {
         case INT:
+        case OPAQUE:
             bucket = *(int*)value % map_size;
             break;
         case POINTER:
