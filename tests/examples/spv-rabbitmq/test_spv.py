1bd5401bd1103ee75dc487f2d3fbdcb9a3feebd6
Peter Gebhard <pgeb@seas.upenn.edu>
2016-10-11 15:43:48 -0400

    Added initial spv test.

---
+++ tests/examples/spv-rabbitmq/test_spv.py
@@ -0,0 +1,16 @@
+import unittest
+import subprocess
+
+class TestSpv(unittest.TestCase):
+
+    def test_mgen(self):
+        mgenCall = subprocess.run()
+
+    def test_compile(self):
+        mgenCall = subprocess.run()
+
+    def test_run(self):
+        mgenCall = subprocess.run()
+
+if __name__ == '__main__':
+    unittest.main()
