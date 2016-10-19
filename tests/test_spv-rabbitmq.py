ca724261ecee9f7791850e48c3ea96f40f52b48a
Peter Gebhard <pgebhard@gmail.com>
2016-10-18 22:29:59 -0400

    Improving spv-rabbitmq test.

--- tests/test_spv-rabbitmq.py
+++ tests/test_spv-rabbitmq.py
@@ -14,44 +14,47 @@ class TestSpvRabbitMQ(unittest.TestCase):
         os.chdir(self.origPath)
 
     def test_spvrabbitmq_mgen(self):
-        call = subprocess.run(["python3", "-m", "smedl.mgen", "--dir", "gen", "tests/examples/spv-rabbitmq/spv"], stdout=subprocess.PIPE)
+        call = subprocess.run(["python3", "-m", "smedl.mgen", "--dir", "gen", "tests/examples/spv-rabbitmq/spv"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     def test_spvrabbitmq_compile(self):
         os.chdir(str(pathlib.PurePath('.', 'tests', 'examples', 'spv-rabbitmq')))
-        call = subprocess.run("make all", shell=True, check=True)
+        call = subprocess.run("make all", shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         self.assertEqual(0, call.returncode)
 
     def test_spvrabbitmq_run(self):
         os.chdir(str(pathlib.PurePath('.', 'tests', 'examples', 'spv-rabbitmq')))
         with open('spv_mon.cfg', 'r') as cfgfile:
-            cfgdata = cfgfile.read().replace('\n', '')
+            cfgdata = cfgfile.read()
         c = cfg.Config(cfgdata)
         credentials = pika.PlainCredentials(c.rabbitmq.username, c.rabbitmq.password)
-        connection = pika.BlockingConnection(pika.ConnectionParameters(
-            c.rabbitmq.hostname, c.rabbitmq.port, '/', credentials))
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters(
+                c.rabbitmq.hostname, c.rabbitmq.port, '/', credentials))
+        except pika.exceptions.ConnectionClosed:
+            self.fail("Could not connect to RabbitMQ server. Is it down?")
         channel = connection.channel()
-        channel.exchange_declare(exchange='logs',
-                                 type='fanout')
-        result = channel.queue_declare(exclusive=True)
-        queue_name = result.method.queue
-        channel.queue_bind(exchange='logs',
-                           queue=queue_name)
 
-        def callback(ch, method, properties, body):
-            print(" [x] %r" % body)
+        # channel.exchange_declare(exchange='logs',
+        #                          type='fanout')
+        # result = channel.queue_declare(exclusive=True)
+        # queue_name = result.method.queue
+        # channel.queue_bind(exchange='logs',
+        #                    queue=queue_name)
+        #
+        # def callback(ch, method, properties, body):
+        #     print(" [x] %r" % body)
+        #
+        # channel.basic_consume(callback,
+        #                       queue=queue_name,
+        #                       no_ack=True)
+        #
+        # channel.start_consuming()
 
-        channel.basic_consume(callback,
-                              queue=queue_name,
-                              no_ack=True)
-
-        channel.start_consuming()
-
-        if rmqserver is down:
-            fail()
-        else:
-            rmqserver listener here
-        call = subprocess.run("./monitor", shell=True, check=True)
-        if rmqserverlistener.received("smedl.control") startswith "Spv monitor" and endswith "started."
+        if connection is None:
+            self.fail()
+        call = subprocess.Popen("./monitor")
+        #if channel.received("smedl.control") startswith "Spv monitor" and endswith "started."
+        call.terminate()
 
 
 if __name__ == '__main__':
