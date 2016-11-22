834a0dafb8587baf978133ae3feb50b75b5c32fb
Peter Gebhard <pgeb@seas.upenn.edu>
2016-11-22 13:58:58 -0500

    SMEDL RC 1.

--- smedl/__about__.py
+++ smedl/__about__.py
@@ -9,7 +9,7 @@ __summary__ = "A tool for generating software monitors from SMEDL and PEDL \
     definitions."
 __uri__ = "https://gitlab.precise.seas.upenn.edu/pgebhard/smedl"
 
-__version__ = "1.0.0.dev5"
+__version__ = "1.0.0.rc1"
 __msg_format_version__ = "1.0.0"
 
 __author__ = "The Trustees of the University of Pennsylvania"
