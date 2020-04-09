6e8ac5606ac3afa3fee5562eb2d73a9dc64fe934
Dominick C. Pastore <dpastore@seas.upenn.edu>
2020-04-09 02:18:01 -0400

    Add most of the rest of the record delete code for maps

--- smedl/codegen/static/local_wrapper.c
+++ smedl/codegen/static/local_wrapper.c
@@ -145,14 +145,17 @@ void monitor_map_insert(SMEDLRecordBase **root, SMEDLRecordBase *rec) {
         /* If the current node's balance is zero, the height of its branch did
          * not change. Balance correction is complete. */
         } while (node->bal != 0);
-
-        /* Insert done and tree is balanced. Return the root. */
-        //TODO Is this necessary? Will the root ever have changed, and can we
-        // update root only when that happens?
-        while (node->parent != NULL) {
-            node = node->parent;
+        
+        /* The root can only change from a rotation. If the current node has
+         * no parent, a rotation may have changed the root. Update it to
+         * the current node. */
+        if (node->parent == NULL) {
+            *root = node;
         }
-        *root = node;
+
+        /* Insert is done, tree is balanced, and root is updated if necessary.
+         * We are done. */
+        return;
     }
 
     /* Found a matching key already present. Add to that key. */
@@ -250,13 +253,13 @@ SMEDLRecordBase * monitor_map_remove(SMEDLRecordBase *rec) {
     /* Only record left with this key. It must be removed from the tree. Do
      * the standard BST deletion: swap the record down to a leaf node in a way
      * that preserves the ordering. */
-    SMEDLRecordBase *parent = NULL;
+    SMEDLRecordBase *node = NULL;
     while (parent == NULL) {
         if (rec->left == NULL) {
             if (rec->right == NULL) {
                 /* Record has no children. Remove it now. */
-                parent = rec->parent;
-                if (parent == NULL) {
+                node = rec->parent;
+                if (node == NULL) {
                     /* Last record in the tree. Return empty tree. */
                     return NULL;
                 }
@@ -279,11 +282,103 @@ SMEDLRecordBase * monitor_map_remove(SMEDLRecordBase *rec) {
         }
     }
 
-    /* TODO Remove the record */
+    /* Remove the record */
+    int bal_change;
+    if (parent->left == rec) {
+        parent->left = NULL;
+        bal_change = 1;
+    } else {
+        parent->right = NULL;
+        bal_change = -1;
+    }
 
-    /* TODO Rebalance */
+    /* Rebalance */
+    do {
+        node->bal += bal_change;
 
-    /* TODO Return the new root */
+        /* Do rotations or prepare parent's balance correction if
+         * necessary */
+        switch (node->bal) {
+            case 0:
+                /* Height of this branch shrunk. Figure out which child we are
+                 * and update bal_change. */
+                if (node->parent == NULL) {
+                    /* At the root. Nothing else to do. */
+                    return node;
+                } else if (node == node->parent->left) {
+                    bal_change = 1;
+                } else {
+                    bal_change = -1;
+                }
+                node = node->parent;
+                break;
+            case -2:
+                /* Needs rebalance to the right */
+                if (node->left->bal < 0) {
+                    /* Left-left case */
+                    node->bal = 0; //CHECK
+                    right_rotate(&node);
+                    node->bal = 0; //CHECK
+                } else {
+                    /* Left-right case */
+                    left_rotate(&node->left);
+                    right_rotate(&node);
+                    if (node->bal < 0) { //CHECK From here...
+                        node->left->bal = 0;
+                        node->right->bal = 1;
+                    } else if (node->bal > 0) {
+                        node->left->bal = -1;
+                        node->right->bal = 0;
+                    } else {
+                        node->left->bal = 0;
+                        node->right->bal = 0;
+                    }
+                    node->bal = 0; //CHECK ...to here
+                }
+                //TODO This copied from the insert code. I believe it is correct
+                // except for possibly the resulting balance factors. Double
+                // check the code marked "CHECK."
+                // Also determine whether rebalance needs to continue.
+                break;
+            case 2:
+                /* Needs rebalance to the left */
+                if (node->left->bal > 0) {
+                    /* Right-right case */
+                    node->bal = 0; //CHECK
+                    node = left_rotate(&node);
+                    node->bal = 0; //CHECK
+                } else {
+                    /* Right-left case */
+                    right_rotate(&node->right);
+                    left_rotate(&node);
+                    if (node->bal < 0) { //CHECK From here...
+                        node->left->bal = 0;
+                        node->right->bal = 1;
+                    } else if (node->bal > 0) {
+                        node->left->bal = -1;
+                        node->right->bal = 0;
+                    } else {
+                        node->left->bal = 0;
+                        node->right->bal = 0;
+                    }
+                    node->bal = 0; //CHECK ...to here
+                }
+                //TODO This copied from the insert code. I believe it is correct
+                // except for possibly the resulting balance factors. Double
+                // check the code marked "CHECK."
+                // Also determine whether rebalance needs to continue.
+                break;
+        }
+    /* If the current node's balance factor is -1 or 1, the height of its branch
+     * did not change (balance factor must have been zero before). Balance
+     * correction is complete. */
+    } while (node->bal != -1 && node->bal != 1);
+
+    /* Return the new root */
+    while (node->parent != NULL) {
+        node = node->parent;
+    }
+    return node;
 }
 
 /* Lookup function
